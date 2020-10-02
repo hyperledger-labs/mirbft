2d1bb539d0463b44dc2331576a84f5c08c53e9c4
Jason Yellick <jyellick@us.ibm.com>
2020-10-02 00:17:04 -0400

    Fix op count in recorder test
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- testengine/recorder_test.go
+++ testengine/recorder_test.go
@@ -76,7 +76,7 @@ var _ = Describe("Recorder", func() {
 		It("Executes and produces a log", func() {
 			count, err := recording.DrainClients(50000)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(count).To(Equal(43806))
+			Expect(count).To(Equal(44010))
 
 			fmt.Printf("Executing test required a log of %d events\n", count)
 
