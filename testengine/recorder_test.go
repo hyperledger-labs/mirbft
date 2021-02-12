c7d0c2b27772163756b78af889bf45e340e7f479
Jason Yellick <jyellick@us.ibm.com>
2021-02-11 22:02:49 -0500

    Fix testengine test failures
    Nothing to see here, determinism changed slightly per code change.
    
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- testengine/recorder_test.go
+++ testengine/recorder_test.go
@@ -68,7 +68,7 @@ var _ = Describe("Recorder", func() {
 		It("Executes and produces a log", func() {
 			count, err := recording.DrainClients(50000)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(count).To(Equal(43930))
+			Expect(count).To(Equal(43922))
 
 			fmt.Printf("Executing test required a log of %d events\n", count)
 
@@ -96,7 +96,7 @@ var _ = Describe("Recorder", func() {
 		It("still executes and produces a log", func() {
 			count, err := recording.DrainClients(100)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(count).To(Equal(63))
+			Expect(count).To(Equal(61))
 		})
 	})
 })
