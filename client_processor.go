00a429e1be46052c8514ee6cdcfe212fc109b866
Jason Yellick <jyellick@us.ibm.com>
2021-02-15 23:39:16 -0500

    Track allocations per client
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- client_processor.go
+++ client_processor.go
@@ -7,8 +7,12 @@ SPDX-License-Identifier: Apache-2.0
 package mirbft
 
 import (
+	"container/list"
 	"context"
 	"fmt"
+	"sync"
+
+	"github.com/pkg/errors"
 
 	pb "github.com/IBM/mirbft/mirbftpb"
 )
@@ -27,17 +31,32 @@ type RequestStore interface {
 type ClientProcessor struct {
 	Node         *Node
 	RequestStore RequestStore
+	clients      map[uint64]*Client
+}
+
+func (cp *ClientProcessor) client(clientID uint64) *Client {
+	if cp.clients == nil {
+		cp.clients = map[uint64]*Client{}
+	}
+
+	c, ok := cp.clients[clientID]
+	if !ok {
+		c = newClient(clientID, cp.RequestStore)
+		cp.clients[clientID] = c
+	}
+	return c
 }
 
 func (cp *ClientProcessor) Process(ca *ClientActions) {
 	for _, r := range ca.AllocatedRequests {
-		digest, err := cp.RequestStore.GetAllocation(r.ClientID, r.ReqNo)
+		client := cp.client(r.ClientID)
+		ack, err := client.allocate(r.ReqNo)
 		if err != nil {
-			panic(fmt.Sprintf("could not get key for %d.%d: %s", r.ClientID, r.ReqNo, err))
+			panic(err)
 		}
 
-		if digest != nil {
-			if err := cp.Node.Propose(context.Background(), r, digest); err != nil {
+		if ack != nil {
+			if err := cp.Node.Propose(context.Background(), r, ack.Digest); err != nil {
 				panic(err)
 			}
 			continue
@@ -80,4 +99,53 @@ func (cp *ClientProcessor) Process(ca *ClientActions) {
 	           }
 	   }
 	*/
+}
+
+type Client struct {
+	mutex        sync.Mutex
+	clientID     uint64
+	requestStore RequestStore
+	requests     *list.List
+	reqNoMap     map[uint64]*list.Element
+}
+
+func newClient(clientID uint64, reqStore RequestStore) *Client {
+	return &Client{
+		clientID:     clientID,
+		requestStore: reqStore,
+		requests:     list.New(),
+		reqNoMap:     map[uint64]*list.Element{},
+	}
+}
+
+type clientRequest struct {
+	localAllocationAck *pb.RequestAck
+	remoteCorrectAcks  []*pb.RequestAck
+}
+
+func (c *Client) allocate(reqNo uint64) (*pb.RequestAck, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	el, ok := c.reqNoMap[reqNo]
+	if ok {
+		clientReq := el.Value.(*clientRequest)
+		return clientReq.localAllocationAck, nil
+	}
+
+	cr := &clientRequest{}
+	el = c.requests.PushBack(cr)
+	c.reqNoMap[reqNo] = el
+
+	digest, err := c.requestStore.GetAllocation(c.clientID, reqNo)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "could not get key for %d.%d", c.clientID, reqNo)
+	}
+
+	cr.localAllocationAck = &pb.RequestAck{
+		ClientId: c.clientID,
+		ReqNo:    reqNo,
+		Digest:   digest,
+	}
+
+	return cr.localAllocationAck, nil
 }
