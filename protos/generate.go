f7af2f3ea1bf7c50f7ddaf88c361bc598f6a70b6
Jason Yellick <jyellick@us.ibm.com>
2021-02-25 13:46:10 -0500

    Split msg and state protos
    Note, nothing consumes these new protos, but they will be moved to over
    the next several commits.
    
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

---
+++ protos/generate.go
@@ -0,0 +1,9 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package protos
+
+//go:generate protoc --proto_path=. --go_out=../pkg/pb/ --go_opt=paths=source_relative msgs/msgs.proto state/state.proto
