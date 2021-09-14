48878c24fc3a95afbc875ad6feae07b250087838
Chrysoula Stathakopoulou <tat@zurich.ibm.com>
2021-09-14 08:45:25 +0200

    Pull only option for remote deployment.
    Signed-off-by: Chrysoula Stathakopoulou <tat@zurich.ibm.com>

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,11 +9,20 @@ clients=$(grep client cloud-instance.info | awk '{ print $1}')
 
 . vars.sh
 
+if [ "$1" = "--pull-only" ] || [ "$1" = "-p" ]; then
+  pull=true
+  shift
+else
+  pull=false
+fi
+
 for p in $servers $clients; do
          pub=$(getIP $p)
-         scp $ssh_options clone.sh $user@$pub:
-         scp $ssh_options install-local.sh $user@$pub:
-         scp $ssh_options vars.sh $user@$pub:
-         ssh $user@$pub $ssh_options "source install-local.sh"
+         if [ "$pull_only" = "false" ]; then
+            scp $ssh_options clone.sh $user@$pub:
+            scp $ssh_options install-local.sh $user@$pub:
+            scp $ssh_options vars.sh $user@$pub:
+            ssh $user@$pub $ssh_options "source install-local.sh"
+         fi
          ssh $user@$pub $ssh_options "source clone.sh"
 done
