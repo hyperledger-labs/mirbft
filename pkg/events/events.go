f047ed41fb62271fd7e76788ae462bf38379c3e2
matejpavlovic <matopavlovic@gmail.com>
2021-09-27 17:56:15 +0200

    Remove 'Event' from the names of event protobufs
    Signed-off-by: matejpavlovic <matopavlovic@gmail.com>

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -36,13 +36,13 @@ func Strip(event *eventpb.Event) *EventList {
 
 // Tick returns an event representing a tick - the event of one step of logical time having elapsed.
 func Tick() *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_Tick{Tick: &eventpb.EventTick{}}}
+	return &eventpb.Event{Type: &eventpb.Event_Tick{Tick: &eventpb.Tick{}}}
 }
 
 // SendMessage returns an event of sending the message message to destinations.
 // destinations is a slice of replica IDs that will be translated to actual addresses later.
 func SendMessage(message *messagepb.Message, destinations []uint64) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_SendMessage{SendMessage: &eventpb.EventSendMessage{
+	return &eventpb.Event{Type: &eventpb.Event_SendMessage{SendMessage: &eventpb.SendMessage{
 		Destinations: destinations,
 		Msg:          message,
 	}}}
@@ -51,7 +51,7 @@ func SendMessage(message *messagepb.Message, destinations []uint64) *eventpb.Eve
 // MessageReceived returns an event representing the reception of a message from another node.
 // The from parameter is the ID of the node the message was received from.
 func MessageReceived(from uint64, message *messagepb.Message) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_MessageReceived{MessageReceived: &eventpb.EventMessageReceived{
+	return &eventpb.Event{Type: &eventpb.Event_MessageReceived{MessageReceived: &eventpb.MessageReceived{
 		From: from,
 		Msg:  message,
 	}}}
@@ -70,7 +70,7 @@ func ClientRequest(clientID uint64, reqNo uint64, data []byte) *eventpb.Event {
 // the origin is an object used to maintain the context for the requesting module and will be included in the
 // HashResult produced by the hashing module.
 func HashRequest(data [][]byte, origin *eventpb.HashOrigin) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_HashRequest{HashRequest: &eventpb.EventHashRequest{
+	return &eventpb.Event{Type: &eventpb.Event_HashRequest{HashRequest: &eventpb.HashRequest{
 		Data:   data,
 		Origin: origin,
 	}}}
@@ -80,7 +80,7 @@ func HashRequest(data [][]byte, origin *eventpb.HashOrigin) *eventpb.Event {
 // It contains the computed digest and the HashOrigin, an object used to maintain the context for the requesting module,
 // i.e., information about what to do with the contained digest.
 func HashResult(digest []byte, origin *eventpb.HashOrigin) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_HashResult{HashResult: &eventpb.EventHashResult{
+	return &eventpb.Event{Type: &eventpb.Event_HashResult{HashResult: &eventpb.HashResult{
 		Digest: digest,
 		Origin: origin,
 	}}}
@@ -89,13 +89,13 @@ func HashResult(digest []byte, origin *eventpb.HashOrigin) *eventpb.Event {
 // RequestReady returns an event signifying that a new request is ready to be inserted into the protocol state machine.
 // This normally occurs when the request has been received, persisted, authenticated, and an authenticator is available.
 func RequestReady(requestRef *messagepb.RequestRef) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_RequestReady{RequestReady: &eventpb.EventRequestReady{
+	return &eventpb.Event{Type: &eventpb.Event_RequestReady{RequestReady: &eventpb.RequestReady{
 		RequestRef: requestRef,
 	}}}
 }
 
 func WALEntry(persistedEvent *eventpb.Event) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_WalEntry{WalEntry: &eventpb.EventWALEntry{
+	return &eventpb.Event{Type: &eventpb.Event_WalEntry{WalEntry: &eventpb.WALEntry{
 		Event: persistedEvent,
 	}}}
 }
@@ -105,14 +105,14 @@ func WALEntry(persistedEvent *eventpb.Event) *eventpb.Event {
 // ============================================================
 
 func PersistDummyBatch(sn uint64, batch *messagepb.Batch) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_PersistDummyBatch{PersistDummyBatch: &eventpb.EventPersistDummyBatch{
+	return &eventpb.Event{Type: &eventpb.Event_PersistDummyBatch{PersistDummyBatch: &eventpb.PersistDummyBatch{
 		Sn:    sn,
 		Batch: batch,
 	}}}
 }
 
 func AnnounceDummyBatch(sn uint64, batch *messagepb.Batch) *eventpb.Event {
-	return &eventpb.Event{Type: &eventpb.Event_AnnounceDummyBatch{AnnounceDummyBatch: &eventpb.EventAnnounceDummyBatch{
+	return &eventpb.Event{Type: &eventpb.Event_AnnounceDummyBatch{AnnounceDummyBatch: &eventpb.AnnounceDummyBatch{
 		Sn:    sn,
 		Batch: batch,
 	}}}
